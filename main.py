d22ced17d4e1d1b00b7f68a4c9516f19adc9cfaa
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2018-07-17 13:51:21 -0700

    Outline necessary functions

---
+++ main.py
@@ -0,0 +1,34 @@
+#Tic Tac Toe Game with AI
+# Grid:
+# 0 1 2
+# 3 4 5
+# 6 7 8
+
+def insertLetter(letter, pos):
+    pass
+
+def spaceIsFree(pos):
+    pass
+
+def printBoard(board):
+    pass
+
+def isWinner(bo, le):
+    pass
+
+def playerMove():
+    pass
+
+def compMove()):
+    pass
+
+def selectRandom(board):
+    pass
+
+def isBoardFull(board):
+    pass
+
+def main():
+    pass
+
+main()
