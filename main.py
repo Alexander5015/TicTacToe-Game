1771976b1499b83a549a4d1f26e8b2f5ee72d05d
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2018-07-17 15:45:46 -0700

    Basic AI

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 #Tic Tac Toe Game with AI
 
-board = [' ' for x in range(10)]
-
-def insertLetter(letter, pos):
+def insertLetter(board, letter, pos):
     board[pos] = letter
 
-def spaceIsFree(pos):
+def spaceIsFree(board, pos):
     return board[pos] == ' '
 
 def printBoard(board):
@@ -33,16 +31,16 @@ def isWinner(board, letter):
     (board[7] == letter and board[5] == letter and board[3] == letter) or # diagonal
     (board[9] == letter and board[5] == letter and board[1] == letter)) # diagonal
 
-def playerMove():
+def playerMove(board):
     run = True
     while run:
         move = input('Please select a position to place an \'X\' (1-9): ')
         try:
             move = int(move)
             if move > 0 and move < 10:
-                if spaceIsFree(move):
+                if spaceIsFree(board, move):
                     run = False;
-                    insertLetter('X', move)
+                    insertLetter(board, 'X', move)
                 else:
                     print('Sorry, this space is occupied!')
             else:
@@ -51,37 +49,97 @@ def playerMove():
             print('Please type a number!')
 
 
-def compMove()):
-    pass
+def compMove(board):
+    #get moves
+    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
+    #set default move
+    move = 0
+
+    #Simple 5 step algorithm
+    #Check if either player can win on a move and move there
+    for letter in ['O', 'X']:
+        for i in possibleMoves:
+            boardCopy = board[:]
+            boardCopy[i] = letter
+            if isWinner(boardCopy, letter):
+                move = i
+                return move
+    #Pick Corner
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1, 3, 7, 9]:
+            cornersOpen.append(i)
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+
+    #Pick Center
+    if 5 in possibleMoves:
+        move = 5;
+        return move
+
+    #Any Open Edge
+    #Pick Corner
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2, 4, 6, 8]:
+            edgesOpen.append(i)
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+        return move
+
+    return move
 
 def selectRandom(board):
-    pass
+    import random
+    rand = random.choice(board)
+    return rand
 
 def isBoardFull(board):
-    return board.count() == 1
+    return board.count(' ') == 1
 
+def clearBoard(board):
+    board = [' ' for x in range(10)]
+    return board
 
 def main():
+    board = [' ' for x in range(10)]
+    playing = True
     print("Welcome to Tic Tac Toe!")
-    printBoard()
-    while not(isBoardFull()):
-        if not(isWinner(board, 'O')):
-            playerMove()
-            printBoard()
+    while playing:
+        printBoard(board)
+        while not(isBoardFull(board)):
+            if not(isWinner(board, 'O')):
+                playerMove(board)
+                printBoard(board)
 
-        else:
-            print('Sorry, O won this time!')
-            break;
+            else:
+                print('Sorry, O won this time!')
+                break;
 
-        if not(isWinner(board, 'X')):
-            compMove()
-            printBoard()
+            if not(isWinner(board, 'X')):
+                move = compMove(board)
+                if move != 0:
+                    insertLetter(board, 'O', move)
+                    print('Computer placed an \'O\' in position', move, ":")
+                    printBoard(board)
 
-        else:
-            print('X won this time! Good Job!')
-            break;
+            else:
+                print('X won this time! Good Job!')
+                break;
 
-    if isBoardFull(board):
-        print("Tie Game!")
+        if isBoardFull(board):
+            print("Tie Game!")
 
+        run = True
+        while run:
+            playAgain = input('Play again? (y/n)')
+            if playAgain.lower() == 'y':
+                run = False
+                board = clearBoard(board)
+            elif playAgain.lower() == 'n':
+                run = False
+                playing = False
+            else:
+                print("\'" + playAgain + "\'" + " is invalid")
 main()
